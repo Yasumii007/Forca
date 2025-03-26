1a94bdceb0274a4905b08be5931cbea07e72abf2
Yasumii007 <yasmimalvesrodrigues16@gmail.com>
2025-03-26 20:54:34 -0300

    Atividade forca, atividade recebe uma palavra pelo usuario e limpa o console logo apos deixando assim o usuario para adivinhar

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string palavra, letra;
+bool possuiLetra;
+
+Console.WriteLine("Bem- vindo ao jogo da forca");
+Console.WriteLine("---- Forca ----");
+
+Console.WriteLine("Por favor digite a palavra secreta");
+palavra = Console.ReadLine()!.ToLower();
+
+Console.Clear();
+
+Console.WriteLine("Digite uma letra: ");
+letra = Console.ReadLine()!.ToLower();
+
+possuiLetra = palavra.Contains(letra);
+
+
+Console.WriteLine($"A letra digitada por você foi \"{letra}\", ela é => {possuiLetra}");
